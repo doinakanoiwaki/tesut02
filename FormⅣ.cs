29692ec1d3090ac4ea2f09c18d43616c74307466
kairi <kairi0318@icloud.com>
2025-10-31 11:55:10 +0900

    パネルを設置した

---
+++ FormⅣ.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tesut02
+{
+    public partial class FormⅣ : Form
+    {
+        public FormⅣ()
+        {
+            InitializeComponent();
+        }
+
+        private void FormⅣ_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
