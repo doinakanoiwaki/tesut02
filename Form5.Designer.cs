8bbf9d0627561300f53cbad5b385884cdc6c7b9f
nakinakiki <kd1380551@kobedenshi078.onmicrosoft.com>
2025-12-04 11:36:27 +0900

    kアカウント画面追加

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -36,12 +36,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(153, 311);
+            this.button1.Location = new System.Drawing.Point(151, 296);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(177, 105);
             this.button1.TabIndex = 0;
             this.button1.Text = "グラフ";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -51,6 +52,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
@@ -59,6 +61,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(250, 59);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
@@ -67,6 +70,7 @@
             this.label1.Size = new System.Drawing.Size(250, 52);
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form5
             // 
--- Form5.cs
+++ Form5.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,8 @@ namespace tesut02
 {
     public partial class Form5 : Form
     {
+        private string registeredName = "";
+
         public Form5()
         {
             InitializeComponent();
@@ -21,5 +24,59 @@ namespace tesut02
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+            private void label1_Click(object sender, EventArgs e)
+        {
+            // 未登録なら注意
+            if (string.IsNullOrEmpty(registeredName))
+            {
+                MessageBox.Show("まだ名前が登録されていません。");
+                return;
+            }
+
+            // 登録した名前を表示
+            label1.Text = registeredName;
+        }
+
+        
+
+            private void button2_Click(object sender, EventArgs e)
+        {
+            // ---- 編集モードに戻す処理 ----
+            if (button2.Text == "編集")
+            {
+                textBox1.Enabled = true;
+                textBox1.BackColor = Color.White;
+                textBox1.BorderStyle = BorderStyle.FixedSingle;
+                button2.Text = "登録";
+                return;
+            }
+
+            // ---- 登録処理 ----
+
+            // 名前を保存
+            registeredName = textBox1.Text;
+
+            // TextBox を透明化＆編集不可に
+            textBox1.Enabled = false;
+            textBox1.BackColor = Color.FromArgb(0, 0, 0, 0); // 透明風
+            textBox1.BorderStyle = BorderStyle.None;
+
+            // ボタンの文字を「編集」に変更
+            button2.Text = "編集";
+        }
+
     }
 }
+
