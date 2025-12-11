bb2e084a09bef8ee89cc614e627b3d93b2f14d97
yaharukidesu <kd1379895@st.kobedenshi.ac.jp>
2025-12-11 14:53:17 +0900

    choi

--- Form3.cs
+++ Form3.cs
@@ -16,81 +16,12 @@ namespace tesut02
     {
         int i = 0;
 
-        string[] Proteinquestions = new string[]
-        {
+        Random rnd = new Random();
 
-            "最近、筋肉量の減少・疲れやすさ・肌や髪のハリの低下などを感じることはありますか？",
+        
 
-            "プロテインやヨーグルト、ゆで卵、チーズなどの高たんぱくなおやつ・サプリを摂ることはありますか？",
 
-            "普段、1日にどのくらい肉・魚・卵・大豆製品などを食べていますか？",
-
-            "食事のときに「たんぱく質を意識して摂ろう」と思うことはありますか？",
-
-            "主食（ご飯やパン）だけで食事を終えることが多いですか？それとも、毎回おかず（肉・   魚・卵・豆腐など）も一緒に食べていますか？\r\n",
-
-        };
-
-        string[] Fatquestions = new string[]
-        {
-
-            "",
-
-            "",
-
-            "",
-
-            "",
-
-            "",
-
-        };
-
-        string[] Carboquestions = new string[]
-        {
-
-            "",
-
-            "",
-
-            "",
-
-            "",
-
-            "",
-
-        };
-
-        string[] Vitaminquestions = new string[]
-        {
-
-            "",
-
-            "",
-
-            "",
-
-            "",
-
-            "",
-
-        };
-
-        string[] Mineralquestions = new string[]
-        {
-
-            "",
-
-            "",
-
-            "",
-
-            "",
-
-            "",
-
-        };
-        private string selectedOption;
+       
 
         public Form3()
         {
@@ -99,26 +30,236 @@ namespace tesut02
 
         private void Form3_Load(object sender, EventArgs e)
         {
-            label1.Text = "普段、1日にどのくらい肉・魚・卵・大豆製品などを食べていますか？";
+            label1.Text = "主食（ご飯やパン）だけで食事を終えることが多いですか？それとも、毎回おかず（肉・   魚・卵・豆腐など）も一緒に食べていますか？";
         }
 
 
-        private void Button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
 
-
+            int x = rnd.Next(5);
+            // 0〜9 のランダムな整数が入る
 
             if (i == 0)
             {
-                label1.Text = "最近、筋肉量の減少・疲れやすさ・肌や髪のハリの低下などを感じることはありますか？\r\n";
+                switch(x)
+                {
+                    case 0:
+                        label1.Text = "最近、筋肉量の減少・疲れやすさ・肌や髪のハリの低下などを感じることはありますか？";
+                        break;
+                    case 1:
+                        label1.Text = "プロテインやヨーグルト、ゆで卵、チーズなどの高たんぱくなおやつ・サプリを摂ることはありますか？";
+                        break;
+                    case 2:
+                        label1.Text = "普段、1日にどのくらい肉・魚・卵・大豆製品などを食べていますか？";
+                        break;
+                    default:
+                        label1.Text = "食事のときに「たんぱく質を意識して摂ろう」と思うことはありますか？";
+                        break;
+                }
             }
             else if (i == 1)
             {
-                label1.Text = "プロテインやヨーグルト、ゆで卵、チーズなどの高たんぱくなおやつ・サプリを摂ることはありますか？\r\n";
-            }
+                switch (x)
+                {
+                    case 0:
+                        label1.Text = "最近、筋肉量の減少・疲れやすさ・肌や髪のハリの低下などを感じることはありますか？";
+                        break;
+                    case 1:
+                        label1.Text = "プロテインやヨーグルト、ゆで卵、チーズなどの高たんぱくなおやつ・サプリを摂ることはありますか？";
+                        break;
+                    case 2:
+                        label1.Text = "普段、1日にどのくらい肉・魚・卵・大豆製品などを食べていますか？";
+                        break;
+                    default:
+                        label1.Text = "食事のときに「たんぱく質を意識して摂ろう」と思うことはありますか？";
+                        break;
+                }
+
+            }    
             else if (i == 2)
             {
-                label1.Text = "（ここに3つ目の質問を追加できます）";
+                switch (x)
+                {
+                    case 0:
+                        label1.Text = "日常的に揚げ物を食べることが多い";
+                        break;
+                    case 1:
+                        label1.Text = "バター、チーズ、卵などを食べることが多い";
+                        break;
+                    case 2:
+                        label1.Text = "脂質が多い肉や加工肉を食べることが多い";
+                        break;
+                    case 3:
+                        label1.Text = "間食でスナック菓子、ナッツ、ケーキなどを食べたりすることが多い\r\n";
+                        break;
+                    default:
+                        label1.Text = "料理やサラダにマヨネーズ、ドレッシング、オリーブオイル、バターなどを\r\n　使うことが多い\r\n";
+                        break;
+
+                }
+            }
+
+            else if (i == 3)
+            {
+                switch (x)
+                {
+                    case 0:
+                        label1.Text = "日常的に揚げ物を食べることが多い";
+                        break;
+                    case 1:
+                        label1.Text = "バター、チーズ、卵などを食べることが多い";
+                        break;
+                    case 2:
+                        label1.Text = "脂質が多い肉や加工肉を食べることが多い";
+                        break;
+                    case 3:
+                        label1.Text = "間食でスナック菓子、ナッツ、ケーキなどを食べたりすることが多い\r\n";
+                        break;
+                    default:
+                        label1.Text = "料理やサラダにマヨネーズ、ドレッシング、オリーブオイル、バターなどを\r\n　使うことが多い\r\n";
+                        break;
+
+                }
+
+            }
+
+            else if (i == 4)
+            {
+                switch (x)
+                {
+                    case 0:
+                        label1.Text = "類・根菜類を食べることが多い";
+                        break;
+                    case 1:
+                        label1.Text = "牛乳・ヨーグルトを食べることが多い";
+                        break;
+                    case 2:
+                        label1.Text = "豆類・種実類を食べることが多い";
+                        break;
+                    case 3:
+                        label1.Text = " 果物類・ドライフルーツを食べることが多い";
+                        break;
+                    default:
+                        label1.Text = "主食が白米、食パン、うどんであることが多い";
+                        break;
+
+                }
+            }
+
+            else if (i == 5)
+            {
+                switch (x)
+                {
+                    case 0:
+                        label1.Text = "類・根菜類を食べることが多い";
+                        break;
+                    case 1:
+                        label1.Text = "牛乳・ヨーグルトを食べることが多い";
+                        break;
+                    case 2:
+                        label1.Text = "豆類・種実類を食べることが多い";
+                        break;
+                    case 3:
+                        label1.Text = " 果物類・ドライフルーツを食べることが多い";
+                        break;
+                    default:
+                        label1.Text = "主食が白米、食パン、うどんであることが多い";
+                        break;
+
+                }
+            }
+
+            else if (i == 6)
+            {
+                switch (x)
+                {
+                    case 0:
+                        label1.Text = "緑黄色野菜はたべますか？";
+                        break;
+                    case 1:
+                        label1.Text = "最近、疲れやすいと感じますか？";
+                        break;
+                    case 2:
+                        label1.Text = "口内炎や唇の荒れが気になりますか？";
+                        break;
+                    case 3:
+                        label1.Text = "風邪をひきやすいと感じますか？";
+                        break;
+                    default:
+                        label1.Text = "外に出る時間が少ないですか？";
+                        break;
+
+                }
+            }
+
+            else if (i == 7)
+            {
+                switch (x)
+                {
+                    case 0:
+                        label1.Text = "緑黄色野菜はたべますか？";
+                        break;
+                    case 1:
+                        label1.Text = "最近、疲れやすいと感じますか？";
+                        break;
+                    case 2:
+                        label1.Text = "口内炎や唇の荒れが気になりますか？";
+                        break;
+                    case 3:
+                        label1.Text = "風邪をひきやすいと感じますか？";
+                        break;
+                    default:
+                        label1.Text = "外に出る時間が少ないですか？";
+                        break;
+
+                }
+            }
+
+            else if (i == 8)
+            {
+                switch (x)
+                {
+                    case 0:
+                        label1.Text = "最近、疲れやすくなったり、だるさを感じることが多い。";
+                        break;
+                    case 1:
+                        label1.Text = "足がつる・こむら返りが起こることが増えた。";
+                        break;
+                    case 2:
+                        label1.Text = "肌荒れや口内炎ができやすくなった。";
+                        break;
+                    case 3:
+                        label1.Text = "集中力が続かず、イライラしやすい。";
+                        break;
+                    default:
+                        label1.Text = "髪の毛が抜けやすくなったり、爪が割れやすくなった。";
+                        break;
+
+                }
+            }
+
+            else if (i == 9)
+            {
+                switch (x)
+                {
+                    case 0:
+                        label1.Text = "最近、疲れやすくなったり、だるさを感じることが多い。";
+                        break;
+                    case 1:
+                        label1.Text = "足がつる・こむら返りが起こることが増えた。";
+                        break;
+                    case 2:
+                        label1.Text = "肌荒れや口内炎ができやすくなった。";
+                        break;
+                    case 3:
+                        label1.Text = "集中力が続かず、イライラしやすい。";
+                        break;
+                    default:
+                        label1.Text = "髪の毛が抜けやすくなったり、爪が割れやすくなった。";
+                        break;
+
+                }
             }
 
 
@@ -141,15 +282,11 @@ namespace tesut02
             this.Hide();
         }
 
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            // FormⅣに選択値を渡して表示
-            FormⅣ FormⅣ = new FormⅣ(selectedOption);
-            FormⅣ.Show();
-        }
-
         
+
+
+
+
     }
 }
 
