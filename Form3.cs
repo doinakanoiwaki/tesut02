08dea4a53fca1baefb0839b32cc4c9e8a6c338c1
yaharukidesu <kd1379895@st.kobedenshi.ac.jp>
2025-12-11 13:27:01 +0900

    a

--- Form3.cs
+++ Form3.cs
@@ -1,4 +1,5 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -142,4 +143,5 @@ namespace tesut02
         
 
     }
-}
+}
+
