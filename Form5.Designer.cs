612efeb72529002939cce80170691663c974bc5d
nakinakiki <kd1380551@kobedenshi078.onmicrosoft.com>
2025-12-17 14:00:20 +0900

    さ

--- Form5.Designer.cs
+++ Form5.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(184, 295);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(110, 400);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(177, 105);
+            this.button1.Size = new System.Drawing.Size(305, 168);
             this.button1.TabIndex = 0;
             this.button1.Text = "グラフ";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,9 +47,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(496, 50);
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Location = new System.Drawing.Point(543, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(169, 80);
+            this.button2.Size = new System.Drawing.Size(333, 152);
             this.button2.TabIndex = 1;
             this.button2.Text = "登録";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,19 +58,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(129, 166);
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Location = new System.Drawing.Point(68, 209);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(264, 79);
+            this.textBox1.Size = new System.Drawing.Size(402, 131);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(123, 50);
+            this.label1.Location = new System.Drawing.Point(62, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(283, 80);
+            this.label1.Size = new System.Drawing.Size(408, 146);
             this.label1.TabIndex = 3;
             this.label1.Text = "↓に名前を入力";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -78,7 +82,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::tesut02.Properties.Resources._cat__wallpaper__pc;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(888, 600);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
