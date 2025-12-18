9d8524d8f321457333a41a89b3afdcd5316f1c95
yaharukidesu <kd1379895@st.kobedenshi.ac.jp>
2025-12-18 11:32:48 +0900

    a

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -40,8 +40,8 @@
             // monthCalendar1
             // 
             this.monthCalendar1.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.monthCalendar1.Location = new System.Drawing.Point(97, 93);
-            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.monthCalendar1.Location = new System.Drawing.Point(121, 112);
+            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(9, 10, 9, 10);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 0;
             // 
@@ -50,10 +50,10 @@
             this.button1.BackColor = System.Drawing.Color.SteelBlue;
             this.button1.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.button1.ForeColor = System.Drawing.Color.Gold;
-            this.button1.Location = new System.Drawing.Point(262, 398);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(328, 478);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(266, 63);
+            this.button1.Size = new System.Drawing.Size(332, 76);
             this.button1.TabIndex = 1;
             this.button1.Text = "質問を開始する";
             this.button1.UseVisualStyleBackColor = false;
@@ -64,10 +64,10 @@
             this.label1.BackColor = System.Drawing.Color.SteelBlue;
             this.label1.Font = new System.Drawing.Font("HG創英角ｺﾞｼｯｸUB", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label1.ForeColor = System.Drawing.Color.Gold;
-            this.label1.Location = new System.Drawing.Point(429, 39);
+            this.label1.Location = new System.Drawing.Point(536, 47);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(278, 312);
+            this.label1.Size = new System.Drawing.Size(348, 374);
             this.label1.TabIndex = 2;
             this.label1.Text = "今日の結果";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -75,19 +75,19 @@
             // label2
             // 
             this.label2.BackColor = System.Drawing.Color.SteelBlue;
-            this.label2.Location = new System.Drawing.Point(43, 39);
+            this.label2.Location = new System.Drawing.Point(54, 47);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(345, 312);
+            this.label2.Size = new System.Drawing.Size(431, 374);
             this.label2.TabIndex = 3;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(717, 10);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(896, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(62, 59);
+            this.pictureBox1.Size = new System.Drawing.Size(78, 71);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -95,18 +95,19 @@
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.ClientSize = new System.Drawing.Size(788, 502);
+            this.ClientSize = new System.Drawing.Size(985, 602);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.label2);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
--- Form2.cs
+++ Form2.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace tesut02
@@ -15,20 +10,73 @@ namespace tesut02
         public Form2()
         {
             InitializeComponent();
+            this.Load += Form2_Load;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form3 form3 = new Form3(); // Form3 のインスタンスを作成
-            form3.Show();              // Form3 を表示
+            Form3 form3 = new Form3();
+            form3.Show();
             this.Hide();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            Form5 form5 = new Form5(); // Form3 のインスタンスを作成
-            form5.Show();              // Form3 を表示
+            Form5 form5 = new Form5();
+            form5.Show();
             this.Hide();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            DesignButton(button1);
+        }
+
+        private void DesignButton(Button btn)
+        {
+            // テキスト（必要なら変更）
+            // btn.Text = "スタート";
+
+            // フォント（おしゃれ寄り・標準で入ってる）
+            btn.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+
+            // ベースカラー（落ち着いたブルー）
+            btn.BackColor = Color.FromArgb(74, 144, 226);
+            btn.ForeColor = Color.White;
+
+            // フラット化（枠線なし）
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.FlatAppearance.BorderSize = 0;
+
+            // ホバー/押下時の色
+            btn.FlatAppearance.MouseOverBackColor = Color.FromArgb(64, 134, 216);
+            btn.FlatAppearance.MouseDownBackColor = Color.FromArgb(54, 124, 206);
+
+            // 角丸（Load時 + サイズ変更時も維持）
+            int radius = 18;
+            btn.Region = new Region(GetRoundPath(btn.ClientRectangle, radius));
+            btn.Resize += (s, e) =>
+            {
+                btn.Region = new Region(GetRoundPath(btn.ClientRectangle, radius));
+            };
+
+            // 文字位置（中央）
+            btn.TextAlign = ContentAlignment.MiddleCenter;
+        }
+
+        private GraphicsPath GetRoundPath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            int d = radius * 2;
+
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, d, d, 180, 90);
+            path.AddArc(rect.Right - d, rect.Y, d, d, 270, 90);
+            path.AddArc(rect.Right - d, rect.Bottom - d, d, d, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - d, d, d, 90, 90);
+            path.CloseFigure();
+
+            return path;
         }
     }
 }
